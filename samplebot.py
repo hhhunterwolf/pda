f76a04c1410f0acf47688ee3bcf2793686e4358f
Matheus Preischadt <matheus.pinheiro@usp.br>
2018-07-14 21:55:01 -0300

    More money. Easier player levels.

--- samplebot.py
+++ samplebot.py
@@ -341,7 +341,7 @@ class Spawn:
 				if victory:
 					Spawn.fought[message.server.id].append(player.pId)
 
-					baseValue = int(3.65*valueMod*(player.level/50)*(wildPokemon.pokeStats.level*255/wildPokemon.captureRate))//3 + 1
+					baseValue = int(6.65*valueMod*(player.level/50)*(wildPokemon.pokeStats.level*255/wildPokemon.captureRate))//3 + 1
 					print('Added EXP: {}'.format())
 					player.addExperience(baseValue)
 					
