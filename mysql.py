e834bb62ead894799c506b6237ca1348de2e07e0
Matheus Preischadt <matheus.pinheiro@usp.br>
2018-08-18 19:38:09 -0300

    Reworked MySQL class to always make a new connection.

--- mysql.py
+++ mysql.py
@@ -1,22 +1,36 @@
 import MySQLdb
 import os
 
+class Cursor:
+    def execute(self, query, tuple=None):
+        cnx = MySQLdb.connect(user=os.environ['MYSQL_USER'], passwd=os.environ['MYSQL_PASS'], host=os.environ['MYSQL_HOST'], db='pda')
+        try:
+            self.cursor = cnx.cursor(MySQLdb.cursors.DictCursor)
+            self.cursor.execute(query, tuple)
+
+        finally:
+            cnx.commit()
+            cnx.close()
+
+    def fetchone(self):
+        return self.cursor.fetchone()
+
+    def fetchall(self):
+        return self.cursor.fetchall()
+
+
 class MySQL:
     cnx = None
 
     @staticmethod
     def getCursor():
-        if not MySQL.cnx:
-            MySQL.cnx = MySQLdb.connect(user=os.environ['MYSQL_USER'], passwd=os.environ['MYSQL_PASS'], host=os.environ['MYSQL_HOST'], db='pda')
+        return Cursor()
 
-        return MySQL.cnx.cursor(MySQLdb.cursors.DictCursor)
-
-
+    # TODO remove these
     @staticmethod
     def commit():
-        MySQL.cnx.commit()
+        return
 
     @staticmethod
     def close():
-        MySQL.cnx.close()
-
+        return
