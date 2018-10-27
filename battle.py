d06cd633f69943c037f177d9bc5231f817cba0a0
Matheus Preischadt <matheus.pinheiro@usp.br>
2018-10-27 19:28:25 -0300

    Changed modifiers for EXP. It is now easier to level up players and pokemon.

--- battle.py
+++ battle.py
@@ -7,7 +7,7 @@ from mysql import MySQL
 
 defaultPower = 75
 defaultCritModifier = 1.5
-expModifier = 2
+expModifier = 2.5
 boostModifier = 0.5
 
 # This should probably be in a utils file. Logs should be done via a log lib. Meh.
