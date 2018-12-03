ef808f9813106d6f9043a1f683adefd7fd744665
Matheus Preischadt <matheus.pinheiro@usp.br>
2018-12-03 17:05:48 +0000

    Converted maintenance to new format.

--- maintenance.py
+++ maintenance.py
@@ -15,10 +15,7 @@ from battle import Battle
 from mysql import MySQL
 from datetime import timedelta
 from pitem import PokeItem
-
-# TODO:
-# Make shop for multiple items
-# Fix pages for exactly %20 pokemon
+from pserver import PokeServer
 
 TOKEN = os.environ['PDA_TOKEN']
 
@@ -28,6 +25,10 @@ playerMessageMap = {}
 messageThreshold = 1.5
 
 playerMap = {}
+
+M_TYPE_INFO = 'INFO'
+M_TYPE_WARNING = 'WARNING'
+M_TYPE_ERROR = 'ERROR'
 
 async def send_maintenance(message):
 	msg = 'Hello {0.author.mention}, PDA is currently under maintenance. It will be back on shortly with new updates! Sorry for the inconvenience, and thank you for your patience.'.format(message)
@@ -44,13 +45,13 @@ async def on_message(message):
 		return
 
 	try:
-		commandPrefix, spawnChannel = serverMap[message.server.id]
+		commandPrefix, spawnChannel = serverMap[message.server.id].get_prefix_spawnchannel()
 	except KeyError as err:
 		return
 
 	content = message.content.lower()
 	if content.startswith(commandPrefix):
-		key = message.author.id + message.server.id
+		key = message.author.id
 		if not key in playerMap:
 			playerMap[key] = Player(key, message.author.name)
 			playerMessageMap[key] = 0
@@ -75,59 +76,17 @@ def evaluate_server(server):
 	commandPrefix = 'p!'
 	spawnChannel = None
 	if row:
-		print('Found server {} in database. Fetching configs.'.format(server.id))
+		print('Found server \'{}\' in database. Fetching configs.'.format(server.id))
 		commandPrefix = row['prefix']
 		spawnChannel = row['spawn_channel']
 	else:
-		print('Server was not {} found in database. Adding.'.format(server.id))
+		print('Server \'{}\' was not found in database. Adding.'.format(server.id))
 		cursor.execute("""
-			INSERT INTO server (id)
-			VALUES (%s)"""
-			, (server.id,))
-
-		cursor.execute("""
-		SELECT * FROM type
-		WHERE id <= 18
-		""")
-
-		rows = cursor.fetchall()
-		for row in rows:
-			random.seed()
-			lastPokemon = random.getrandbits(24)
-
-			cursor.execute("""
-				SELECT *
-				FROM (
-					SELECT pokemon.id as pokemon_id, SUM(base_stat) as sum_var
-				  FROM type JOIN pokemon_type JOIN pokemon JOIN pokemon_stat
-				  WHERE pokemon_type.type_id = type.id
-				  AND pokemon_type.pokemon_id = pokemon.id
-				  AND pokemon_stat.pokemon_id = pokemon.id
-				  AND type.id = %s
-				  AND pokemon_type.pokemon_id < 722
-				  GROUP BY pokemon.id
-				) as temp
-				WHERE sum_var > 450
-				ORDER BY RAND()
-				LIMIT 1
-				""", (row['id'],))
-			rowPokemon = cursor.fetchone()
-
-			pokemon = Pokemon(name='', pokemonId=rowPokemon['pokemon_id'], level=100)
-			
-			cursor.execute("""
-				INSERT INTO player_pokemon (id, player_id, pokemon_id, level, experience, current_hp, iv_hp, iv_attack, iv_defense, iv_special_attack, iv_special_defense, iv_speed, selected, caught_with)
-				VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-				""", (lastPokemon, "PDA", pokemon.pId, pokemon.pokeStats.level, pokemon.experience, pokemon.pokeStats.hp, pokemon.pokeStats.iv['hp'], pokemon.pokeStats.iv['attack'], pokemon.pokeStats.iv['defense'], pokemon.pokeStats.iv['special-attack'], pokemon.pokeStats.iv['special-defense'], pokemon.pokeStats.iv['speed'], 0, pokemon.caughtWith))		
-
-			cursor.execute("""
-				INSERT INTO gym (server_id, type_id, holder_id, pokemon_id)
-				VALUES (%s, %s, 'PDA', %s)
-				""", (server.id, row['id'], lastPokemon))
-
-		MySQL.commit()
-
-	serverMap[server.id] = [commandPrefix, spawnChannel]
+		INSERT INTO server (id)
+		VALUES (%s)"""
+		, (server.id,))
+		
+	serverMap[server.id] = PokeServer(id=server.id, commandPrefix=commandPrefix.lower(), spawnChannel=spawnChannel)
 	print('Done.')
 	
 @client.event
